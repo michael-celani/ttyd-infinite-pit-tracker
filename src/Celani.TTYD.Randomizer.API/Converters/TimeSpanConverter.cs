ca20f01daa223269f24d824232ec4c44ad896bf3
Michael Celani <michael.j.celani@gmail.com>
2024-03-07 00:25:38 -0600

    Avoid unnecessary allocations

--- src/Celani.TTYD.Randomizer.API/Converters/TimeSpanConverter.cs
+++ src/Celani.TTYD.Randomizer.API/Converters/TimeSpanConverter.cs
@@ -13,14 +13,22 @@ namespace Celani.TTYD.Randomizer.API.Converters
 
         public override void Write(Utf8JsonWriter writer, TimeSpan value, JsonSerializerOptions options)
         {
+            if (value.TotalHours <= 0)
+            {
+                writer.WriteStringValue("00:00:00.00");
+                return;
+            }
+
             if (value.TotalHours >= 100)
             {
                 writer.WriteStringValue("99:59:59.99");
                 return;
             }
-
-            var val = $@"{(int) value.TotalHours}:{value:mm\:ss\.ff}";
-            writer.WriteStringValue(val);
+            
+            Span<char> arr = stackalloc char[11];
+            value.TotalHours.TryFormat(arr[0..2], out var charsWritten, "00");
+            value.TryFormat(arr[2..], out charsWritten, @"\:mm\:ss\.ff");
+            writer.WriteStringValue(arr);
         }
     }
 }
